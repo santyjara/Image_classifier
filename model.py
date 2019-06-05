bc6457031bf5b772872b871708edfac699b721a4
rubelio cardona <rucardona16@gmail.com>
2019-06-04 21:09:07 -0500

    cambio en la funcion getLinealModel

--- model.py
+++ model.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
 
-def getLinealModel(num_class, learning_rate):
+def getLinealModel(num_class):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
     model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
-    model.compile(loss=tf.losses.SparseCategoricalCrossentropy(),
-                optimizer=tf.optimizers.Adam(learning_rate),
-                metrics=['accuracy'])
     return model
